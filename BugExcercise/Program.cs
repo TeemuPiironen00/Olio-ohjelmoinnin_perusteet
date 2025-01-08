bcb6d31d31008f9a4a2ca47e405f59fd6a490b93
Teemu Piironen <Teemu.Piironen@student.lab.fi>
2025-01-08 15:12:18 +0200

    student class version

--- BugExcercise/Program.cs
+++ BugExcercise/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        Student studentA = new Student();
+                studentA.firstName = "Anni";
+                studentA.lastName = "Piiska";
+                studentA.SetAge(18);
+            studentA.PrintInformation();    
+            //Console.WriteLine($"Etunimi: {studentA.firstName}, Sukunimi: {studentA.lastName}, {studentA.age}");
+            //studentA.PrintInformation();
+            Student studentB = new Student();
+                studentB.firstName = "Mikko";
+                studentB.lastName = "Puuska";
+                studentB.SetAge(12);
+                studentB.PrintInformation();
+                //Console.WriteLine($"Etunimi: {studentB.firstName}, Sukunimi: {studentB.lastName}, {studentB.age}");
+            Student studentC = new Student("Maisa", "Karisaari", 19);
+            studentC.PrintInformation();
         }
     }
 }
---
+++ BugExcercise/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugExcercise
+{
+    internal class Student
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public int id;
+        public string notes;
+
+        public Student(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.notes = "";
+            SetAge(age);
+        }
+        public Student()
+        {
+            this.firstName = "";
+            this.lastName = "";
+            this.age = 0;
+            this.notes = "";
+        }
+
+        internal void PrintInformation()
+        {
+            // Print student info to console
+            Console.WriteLine($"Etunimi: {this.firstName}, Sukunimi: {this.lastName}, Ikä {this.age}, huom: {this.notes} ");
+            
+        }
+
+        public void SetAge(int age)
+        {
+            if (age < 18)
+            {
+                this.notes = "Tarkista opiskelijan ikä.";
+            }
+
+            this.age = age;        
+        }
+
+    }
+
+}
