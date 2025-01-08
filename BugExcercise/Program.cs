009f559d10f88c16c0968b100645d421a549f456
TeemuPiironen00 <teemudanielpiironen@gmail.com>
2025-01-08 18:25:57 +0200

    Update Program.cs

--- BugExcercise/Program.cs
+++ BugExcercise/Program.cs
@@ -1,23 +1,23 @@
-﻿namespace BugExcercise
+namespace BugExcercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-        Student studentA = new Student();
-                studentA.firstName = "Anni";
-                studentA.lastName = "Piiska";
-                studentA.SetAge(18);
-            studentA.PrintInformation();    
+            Student studentA = new Student();
+            studentA.firstName = "Anni";
+            studentA.lastName = "Piiska";
+            studentA.SetAge(18);
+            studentA.PrintInformation();
             //Console.WriteLine($"Etunimi: {studentA.firstName}, Sukunimi: {studentA.lastName}, {studentA.age}");
             //studentA.PrintInformation();
             Student studentB = new Student();
-                studentB.firstName = "Mikko";
-                studentB.lastName = "Puuska";
-                studentB.SetAge(12);
-                studentB.PrintInformation();
-                //Console.WriteLine($"Etunimi: {studentB.firstName}, Sukunimi: {studentB.lastName}, {studentB.age}");
+            studentB.firstName = "Mikko";
+            studentB.lastName = "Puuska";
+            studentB.SetAge(12);
+            studentB.PrintInformation();
+            //Console.WriteLine($"Etunimi: {studentB.firstName}, Sukunimi: {studentB.lastName}, {studentB.age}");
             Student studentC = new Student("Maisa", "Karisaari", 19);
             studentC.PrintInformation();
         }
