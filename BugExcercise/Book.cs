3af11c32f5b894c2b32531f6c788e86cc98a060d
Teemu Piironen <Teemu.Piironen@student.lab.fi>
2025-01-09 13:26:39 +0200

    Classroom final version

--- BugExcercise/Book.cs
+++ BugExcercise/Book.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BugExcercise
+namespace BookExcercise
 {
     internal class Book
     {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+
+        public void SetId(string idValue)
+        {
+            this.id = idValue;
+            if (idValue.Length > 5)
+            {
+                Console.WriteLine(this.title + " id virheellinen");
+
+            }
+        }
+
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            SetId(id);
+            this.price = price;
+        }
+
+        internal void BookPrint()
+        {
+            Console.WriteLine($"title: {this.title}, author: {this.author}, id: {this.id}, price: {this.price}");
+        }
+
+        public void CompareBook(Book book2)
+        {
+            if (this.price < book2.price)
+            {
+                Console.WriteLine("Book1 is cheaper");
+            }
+            else if (this.price > book2.price)
+            {
+                Console.WriteLine("Book2 is cheaper");
+            }
+            else
+            {
+                Console.WriteLine("Books are the same price");
+            }
+        }
     }
 }
--- BugExcercise/Program.cs
+++ BugExcercise/Program.cs
@@ -1,25 +1,41 @@
-﻿namespace BugExcercise
+﻿namespace BookExcercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        Student studentA = new Student();
-                studentA.firstName = "Anni";
-                studentA.lastName = "Piiska";
-                studentA.SetAge(18);
-            studentA.PrintInformation();    
+            /*Console.WriteLine("Hello, World!");
+            Student studentA = new Student();
+            studentA.firstName = "Anni";
+            studentA.lastName = "Piiska";
+            studentA.SetAge(18);
+            studentA.PrintInformation();
             //Console.WriteLine($"Etunimi: {studentA.firstName}, Sukunimi: {studentA.lastName}, {studentA.age}");
             //studentA.PrintInformation();
             Student studentB = new Student();
-                studentB.firstName = "Mikko";
-                studentB.lastName = "Puuska";
-                studentB.SetAge(12);
-                studentB.PrintInformation();
-                //Console.WriteLine($"Etunimi: {studentB.firstName}, Sukunimi: {studentB.lastName}, {studentB.age}");
+            studentB.firstName = "Mikko";
+            studentB.lastName = "Puuska";
+            studentB.SetAge(12);
+            studentB.PrintInformation();
+            //Console.WriteLine($"Etunimi: {studentB.firstName}, Sukunimi: {studentB.lastName}, {studentB.age}");
             Student studentC = new Student("Maisa", "Karisaari", 19);
             studentC.PrintInformation();
+            */
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+            Book book1 = new Book("Maisan maailma", "Liisu.M", "02230", 23);
+            Book book2 = new Book("Lissun Linna", "Maija.M", "10000", 25);
+
+            book1.BookPrint();
+            Console.WriteLine();
+            book2.BookPrint();
+
+            book1.CompareBook(book2);
+
+
+
+
         }
     }
 }
