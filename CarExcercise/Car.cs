74f7fd8be9ed6ac8779ad68fe2abbbaa9f898fb8
Teemu Piironen <Teemu.Piironen@student.lab.fi>
2025-01-09 14:56:28 +0200

    Classroom work

---
+++ CarExcercise/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarExcercise
+{
+    internal class Car
+    {
+        public string? brand;
+        public int? speed;
+
+    public Car()
+        {
+            this.brand = "";
+            this.speed = 0;
+        }
+
+        internal void Accelerate()
+        {
+            Console.WriteLine("Paljonko nopeutta lisätään?");
+            int speed1 = 0;
+            int.TryParse(Console.ReadLine(), out speed1);
+            this.speed += speed1;
+            Console.WriteLine(this.brand + ":n nopeus muutoksen jälkeen on " + this.speed);
+        }
+        internal void AskData()
+        {
+            Console.WriteLine("merkki");
+            this.brand = Console.ReadLine();
+            Console.WriteLine("nopeus");
+            this.speed = int.Parse(Console.ReadLine());
+        }
+
+        internal void ShowCarInfo()
+        {
+            Console.WriteLine($"brand: {this.brand}, speed: {this.speed}, ");
+        }
+    }
+
+    
+
+
+
+
+}
--- CarExcercise/Program.cs
+++ CarExcercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            Car car2 = new Car();
+
+
+
+            car1.AskData();
+            Console.WriteLine();
+            car1.ShowCarInfo();
+            Console.WriteLine();
+            
+            car2.AskData();
+            Console.WriteLine();
+            car2.ShowCarInfo();
+            Console.WriteLine();
+
+            car1.Accelerate();
+            Console.WriteLine();
+            car2.Accelerate();
+
+
+
+
+
+
         }
     }
 }
