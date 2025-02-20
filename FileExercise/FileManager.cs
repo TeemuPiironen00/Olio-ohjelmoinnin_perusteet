2241ca429feb4d83064b0a1a73ef140f200a85ec
Teemu Piironen <Teemu.Piironen@student.lab.fi>
2025-02-20 11:15:00 +0200

    Classroom entry

---
+++ FileExercise/FileManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FileExercise
+{
+    // Custom exception
+    public class WordListNotFoundException : Exception
+    {
+        public WordListNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+
+    class FileManager
+    {
+        private string filePath;
+
+        // Constructor
+        public FileManager()
+        {
+            filePath = string.Empty;
+        }
+
+        // Constructor
+        public FileManager(string filePath)
+        {
+            this.filePath = filePath;
+          
+        }
+
+        public string ReadWords()
+        {
+            string content = string.Empty;
+
+            content = ReadFile();
+
+            return content;
+        }
+
+        /* Method reads file from a local 
+         * directory path.
+         */
+        private string ReadFile()
+        {
+            if (!File.Exists(filePath))
+            {
+                // Throw custom exception if file does not exist
+                throw new WordListNotFoundException("File not available");
+            }
+
+            string directoryName = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileName(filePath);
+            string fileExtension = Path.GetExtension(filePath);
+
+            Console.WriteLine("directoryName: " + directoryName);
+            Console.WriteLine("fileName: " + fileName);
+            Console.WriteLine("fileExtension: " + fileExtension);
+
+            string fileContent = File.ReadAllText(filePath, Encoding.UTF8);
+            return fileContent;
+        }
+    }
+}
---
+++ FileExercise/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FileExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileExample();
+            WriteJsonFile();
+            ReadJsonFile();
+        }
+
+        static void WriteJsonFile()
+        {
+            const string PATH = @"C:\Users\toxii\Desktop\Ohjelmointi\Olio-ohjelmointi perusteet\Olio-ohjelmoinnin_perusteet\FileExercise\testi.json";
+            List<Book> books = new List<Book>
+                {
+                    new Book { Title = "Maa", Author = "Juhani Aho", price = 91 },
+                    new Book { Title = "Seitsemän veljestä", Author = "Aleksis Kivi", price = 18 },
+                    new Book { Title = "Tuntematon sotilas", Author = "Väinö Linna", price = 19 },
+                    new Book { Title = "Sinuhe egyptiläinen", Author = "Mika Waltari", price = 19 },
+                    new Book { Title = "Punainen viiva", Author = "Ilmari Kianto", price = 12 }
+                };
+
+
+            File.WriteAllText(PATH, JsonConvert.SerializeObject(books));
+        }
+
+        static void ReadJsonFile()
+        {
+            const string PATH = @"C:\Users\toxii\Desktop\Ohjelmointi\Olio-ohjelmointi perusteet\Olio-ohjelmoinnin_perusteet\FileExercise\testi.json";
+            string json = File.ReadAllText(PATH, System.Text.Encoding.UTF8);
+            List<Book> books = JsonConvert.DeserializeObject<List<Book>>(json);
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Price: {book.price}");
+            }
+
+        }
+
+            static void FileExample()
+        {
+            string filePath = @"C:\Users\toxii\Desktop\Ohjelmointi\Olio-ohjelmointi perusteet\Olio-ohjelmoinnin_perusteet\FileExercise\testi.txt";
+
+            try
+            {
+                FileManager fm = new FileManager(filePath);
+                Console.WriteLine(fm.ReadWords());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int price { get; set; }
+        }
+    }
+}
